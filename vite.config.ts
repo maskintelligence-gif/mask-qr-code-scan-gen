2736ebfbe7f10426da7cc60e51d6c3e431b9dc0f
MASK INTELLIGENCE <maskintelligence@gmail.com>
2026-01-11 00:56:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' 
+    ? '/mask-qr-code-scan-gen/'
+    : '/',
+})
       },
       plugins: [react()],
       define: {
